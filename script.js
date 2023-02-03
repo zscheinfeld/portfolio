abfa7d994ba7d488a3a83aa71a4ee29c6eb6db55
zscheinfeld <zachscheinfeld@Zachs-MacBook-Pro-2.local>
2023-02-03 17:01:49 -0500

    newpush

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ var bgmode= 0;
 
 $.getJSON("projects.json", function(grid) {
 
-    var gridheight = $(window).height() - $(".topnav").height() - $(".tags").height()
+    if ($(window).width() <720){
+        var greenheight = window.innerHeight - 60
+        // console.log(greenheight)
+        $(".bgspace").height(greenheight)
+        $(".bggrid2").height(window.innerHeight)
+ 
+        // $()
+    }
+
+    // var gridheight = $(window).innerHeight() - $(".topnav").height() - $(".tags").height()
+    var gridheight = $(window).innerHeight() - $(".topnav").height() - $(".tags").height()
     
 
     $(".columns").height(gridheight)
@@ -22,6 +32,7 @@ $.getJSON("projects.json", function(grid) {
     var link='';
     var haslink = 0;
    
+    function fillgrid(){
     $.each(grid, function(i, project) {
         if(project.link.length == 0){
             link=''
@@ -50,29 +61,14 @@ $.getJSON("projects.json", function(grid) {
         <div class="columnitemtext">${project.name}</div>
         <div class="columnitemtext">${project.year}</div>
         </div><div class="caption" id="caption${project.item}"=>${project.caption}<div class="link">${link}</div><div class="captiontags">${imagetags}</div></div>`
-
-        // if(haslink==0){
-        // leftcolumn += 
-        // `<div class="columnitem" id="label${project.item}">
-        // <div class="columnitemtext">${project.name}</div>
-        // <div class="columnitemtext">${project.year}</div>
-        // </div><div class="caption" id="caption${project.item}"=>${project.caption}<div class="captiontags">${imagetags}</div></div>`
-        // }
-
-        // else if(haslink==1){
-        // leftcolumn += 
-        // `<div class="columnitem" id="label${project.item}">
-        // <div class="columnitemtext">${project.name}</div>
-        // <div class="columnitemtext">${project.year}</div>
-        // </div><div>${link}</div><div class="caption" id="caption${project.item}"=>${project.caption}<div class="captiontags">${imagetags}</div></div>`
-
-        // }
         
         if (`${project.column}`== "1"){
             column1 +=  
             `<div class="columnimage" id="image${project.item}">
             <div class="hidearrow arrows" ><div class="leftclick"></div><div class="rightclick"></div></div>
+            <a href='#${project.item}'>
             <img src= ${project.image}>
+            </a>
             </div>`
         }
 
@@ -80,7 +76,9 @@ $.getJSON("projects.json", function(grid) {
             column2 +=  
            `<div class="columnimage" id="image${project.item}">
             <div class="hidearrow arrows"><div class="leftclick"></div><div class="rightclick"></div></div>
+            <a href='#${project.item}'>
             <img src= ${project.image}>
+            </a>
             </div>`
         }
 
@@ -88,11 +86,15 @@ $.getJSON("projects.json", function(grid) {
             column3 +=  
             `<div class="columnimage" id="image${project.item}">
             <div class="hidearrow arrows"><div class="leftclick"></div><div class="rightclick"></div></div>
+            <a href='#${project.item}'>
             <img src= ${project.image}>
+            </a>
             </div>`
         }
         
-    });
+    })}
+
+    fillgrid();
 
     
 
@@ -141,7 +143,7 @@ $.getJSON("projects.json", function(grid) {
         
         $( ".columnimage" ).each(function(i) {
             if ($(this).attr('id')== clicked){
-                $(':nth-child(2)', this).attr('src',`${grid[clickedno-1].images[slideno]}`)
+                $(':nth-child(2)', this).children().attr('src',`${grid[clickedno-1].images[slideno]}`)
             }
             else{
                 $(this).hide()
@@ -188,7 +190,6 @@ $.getJSON("projects.json", function(grid) {
         slideshow = 1
         clicked = $(this).attr('id')
         clickedno = clicked.slice(-2)
-        
 
         $(".arrows").removeClass("hidearrow");
         
@@ -239,76 +240,96 @@ $.getJSON("projects.json", function(grid) {
 
     var resetmode = 0;
 
-    $("#projectreset").click(function(){
-        function gridreset(){
-            $(".arrows").addClass("hidearrow");
-            slideshow = 0
-            slideno = 0
-            var selection = "#"+`${clicked}`
-            $("#"+`${clicked}`).children().attr('src',`${grid[clickedno-1].image}`)
-            $( ".rightcolumns" ).each(function(i) {
-                if($(this).attr('id')== parentid){
-                    $(this).css("flex","25%")
-                }
-    
-                else{
-                    $(this).css("flex","25%")
-                    $(this).show()
-                }
-            
-            });    
-    
-            $( ".columnimage" ).each(function(i) {
-                if ($(this).attr('id')== clicked){
-                   
-                }
-                else{
-                    $(this).show()
-                }
-              });
-    
-              $(".leftcolumn").children().each(function(i) {
+    function exitslideshow(){
+        console.log("cicked")
+        $(".arrows").addClass("hidearrow");
+        slideshow = 0
+        slideno = 0
+        var selection = "#"+`${clicked}`
+
+        $("#"+`${clicked}`).children().attr('src',`${grid[clickedno-1].image}`)
+        $( ".rightcolumns" ).each(function(i) {
+            if($(this).attr('id')== parentid){
+               
+                $(this).css("flex","33.33%")
+            }
+
+            else{
+                $(this).css("flex","33.33%")
                 $(this).show()
-                $(this).addClass("columnitem")
-                $(this).removeClass("descriptionitem")
-              });
-    
-              $(".caption").each(function(i) {
-                $(this).hide()
-              });
-        }
+            }
+        
+        });    
+
+        $( ".columnimage" ).each(function(i) {
+            if ($(this).attr('id')== clicked){
+               
+            }
+            else{
+                $(this).show()
+            }
+          });
+
+          $(".leftcolumn").children().each(function(i) {
+            $(this).show()
+            $(this).addClass("columnitem")
+            $(this).removeClass("descriptionitem")
+          });
+
+          $(".caption").each(function(i) {
+            $(this).hide()
+          });
+    }
+
+    $("#projectreset").click(function(){
+        console.log(resetmode)
         if (resetmode == 0){
-            console.log("reset 0")
             // $(".bgspace").show();
             // $(".bggrid").show();
             // $(window).scrollTop(scrolltoheight)
-            $("html, body").animate({ scrollTop: scrolltoheight},1050, "swing");
+            if ($(window).width() <720){
+                scrolltoheight - window.pageYOffset
+                console.log(window.pageYOffset)
+            }
+
+            $("html, body").animate({ scrollTop: scrolltoheight}, 1300, "swing");
             setTimeout(() => {
             $(".bgspace").hide();
             $(".bggrid").hide();
             resetmode = 1
-        }, "1100")
+        }, "1200")
 
         }
 
-        else if(resetmode == 1){
-            gridreset();
-
-            }
+        else if (resetmode == 1){
+            exitslideshow();
+            resetmode = 1
            
-
-        // if reset mode = 2 (AKA tags have been activated)
-
-        else if(resetmode = 2){
-            turnedontagsarray =[]
-            projectarray = [];
-            detachedprojectarray = [];
-            columncounter = 1;
-            tagson= 0; 
-            
         }
 
-        // if reset mode = 3 (full grid, nothing happens)
+        else if (resetmode == 2){
+            exitslideshow();
+            columncounter = 1;
+            console.log(projectarray)
+            detachedprojectarray = $(".rightcolumns").children().detach()
+            for(x=0;x< projectarray.length; x++){
+                var columnid = "#column" + `${columncounter}`
+                $(columnid).append(projectarray[x])
+                 if(columncounter == 3){
+                        columncounter = 1
+                    }
+                    else{
+                        columncounter = columncounter + 1
+                    }
+            }
+            turnedontagsarray =[]
+            tagson= 0;
+            detachedprojectarray = [];
+            $(".tagselect").addClass("tag")
+            $(".tagselect").removeClass("tagselect")
+            resetmode = 1
+
+        }
 
     
     })
@@ -337,56 +358,66 @@ $.getJSON("projects.json", function(grid) {
     var projectno; 
     var tagson= 0; 
     
-    $(".tag").click(function(){    
-        resetmode = 2
-        var turnedonprojects = []
-        tagson = tagson + 1 
-
-        if (turnedontagsarray.includes($(this).attr('id'))){
-            index = turnedontagsarray.indexOf($(this).attr('id'));
-            turnedontagsarray.splice(index,1)
-
+    $(".tag").click(function(){ 
+        resetmode = 2 
+        if(slideshow == 1){
+            exitslideshow();
+            slideshow = 0
         }
-        else{
-            turnedontagsarray.push($(this).attr('id'))
-        }
-        $(this).toggleClass("tag")
-        $(this).toggleClass("tagselect")
-
-        $.each(grid, function(i, project) {
-            projectno = $(this)[0].item;
-            if(tagson < 2){
-                projectarray[i]= $("#image" + `${projectno}`).detach();
+        // make reset button remember that tags have been turned on 
+        
+            var turnedonprojects = []
+            tagson = tagson + 1 
+    
+            if (turnedontagsarray.includes($(this).attr('id'))){
+                index = turnedontagsarray.indexOf($(this).attr('id'));
+                turnedontagsarray.splice(index,1)
+    
             }
             else{
-                detachedprojectarray = $(".rightcolumns").children().detach()
+                turnedontagsarray.push($(this).attr('id'))
             }
-            for(x=0; x< $(this)[0].tags.length ; x++){ 
-                if((turnedontagsarray.includes($(this)[0].tags[x])) == true){
-                    if(turnedonprojects.includes(projectarray[i]) == true){
-                    }
-                    else{
-                        turnedonprojects.push(projectarray[i])
-                    }
-                    continue;          
+            $(this).toggleClass("tag")
+            $(this).toggleClass("tagselect")
+            $.each(grid, function(i, project) {
+                projectno = $(this)[0].item;
+                if(tagson < 2){
+                    projectarray[i]= $("#image" + `${projectno}`).detach();
                 }
-            }
-        })
-
-        var columncounter = 1;
-
-        for(x=0;x< turnedonprojects.length; x++){
-            var columnid = "#column" + `${columncounter}`
-            $(columnid).append(turnedonprojects[x])
-             if(columncounter == 3){
-                    columncounter = 1
-                }
-
                 else{
-                    columncounter = columncounter + 1
+                    detachedprojectarray = $(".rightcolumns").children().detach()
                 }
+                for(x=0; x< $(this)[0].tags.length ; x++){ 
+                    if((turnedontagsarray.includes($(this)[0].tags[x])) == true){
+                        if(turnedonprojects.includes(projectarray[i]) == true){
+                        }
+                        else{
+                            turnedonprojects.push(projectarray[i])
+                        }
+                        continue;          
+                    }
+                }
+            })
+    
+            var columncounter = 1;
+    
+            for(x=0;x< turnedonprojects.length; x++){
+                var columnid = "#column" + `${columncounter}`
+                $(columnid).append(turnedonprojects[x])
+                 if(columncounter == 3){
+                        columncounter = 1
+                    }
+    
+                    else{
+                        columncounter = columncounter + 1
+                    }
+    
+            }
+            
+        
 
-        }
+
+        
 
 
     
@@ -402,13 +433,18 @@ $.getJSON("projects.json", function(grid) {
     // scroll
     var fired = 0; 
     var aboutclicked = 0; 
-    var scrolltoheight = $(window).height()
+    var scrolltoheight = window.innerHeight 
+
+    $( window ).resize(function() {
+        scrolltoheight = window.innerHeight 
+        $(".bggrid2").height(window.innerHeight)
+      });
+
     window.onbeforeunload = function () {
         window.scrollTo(0, 0);
         fired=0;
       }
     $(document).scroll(function(){
-
         if($(window).scrollTop()> (.7 * scrolltoheight)){
             if((fired == 0) && (aboutclicked ==0)){
                 console.log("fired")
